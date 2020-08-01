ddff0910072d88a02ff0bad0f6b756e08ce4ba4a
SiliconSquaredDev <57053859+Si2Dev@users.noreply.github.com>
2020-08-01 14:31:23 -0700

    Creation

---
+++ main.ts
@@ -0,0 +1,21 @@
+/**
+ * Functions to operate NeoPixel strips.
+ */
+//% weight=5 color=#2699BF icon="\uf110"
+namespace ExpressivePixels {
+    /**
+     * Create a new NeoPixel driver for `numleds` LEDs.
+     * @param pin the pin where the colorbit is connected.
+     * @param numleds number of leds in the strip, eg: 25
+     */
+    //% blockId="colorbit_create" block="NeoPixel at pin %pin|with %numleds|leds as %mode"
+    //% weight=90 blockGap=8
+    //% parts="colorbit"
+    //% trackArgs=0,2
+    //% blockSetVariable=colorbit_51bit
+    //% advanced=true
+    export function Create(strip: neopixel.Strip , buf: Buffer)
+        
+    }
+
+}
