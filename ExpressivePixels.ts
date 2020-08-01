41620edb4405039499302bffec817c4e057ebf1d
SiliconSquaredDev <57053859+Si2Dev@users.noreply.github.com>
2020-08-01 15:09:18 -0700

    up

--- ExpressivePixels.ts
+++ ExpressivePixels.ts
@@ -8,14 +8,26 @@ namespace ExpressivePixels {
      * @param pin the pin where the colorbit is connected.
      * @param numleds number of leds in the strip, eg: 25
      */
-    //% blockId="colorbit_create" block="NeoPixel at pin %pin|with %numleds|leds as %mode"
+    //% blockId="ExpressivePixels_create" block="NeoPixel at pin %pin|with %numleds|leds as %mode"
     //% weight=90 blockGap=8
-    //% parts="colorbit"
+    //% parts="ExpressivePixels"
     //% trackArgs=0,2
-    //% blockSetVariable=colorbit_51bit
+    //% blockSetVariable=leddisplay
     //% advanced=true
-    export function Create(strip: neopixel.Strip , buf: Buffer)
+    export function create(strip: string , buf: Buffer)
         
     }
+
+
+        /**
+         * Turn off all LEDs.
+         * You need to call ``show`` to make the changes visible.
+         */
+        //% blockId="ExpressivePixels_clear" block="%leddisplay|clear"
+        //% weight=76
+        //% parts="ExpressivePixels"
+        clear(): void {
+        }
+
 
 }
